07e9c31d1a816ff7ad1ad806c2c3e095ee827357
Sim Young Jae <hanul@hanul.me>
2022-06-16 17:18:37 +0900

    minter

--- contracts/YabaiGoblin.sol
+++ contracts/YabaiGoblin.sol
@@ -7,8 +7,10 @@ import "./ERC721A/ERC721A.sol";
 contract YabaiGoblin is Ownable, ERC721A {
 
     event SetBaseURI(string uri);
+    event SetMinter(address minter);
 
     string internal __baseURI;
+    address public minter;
 
     constructor() ERC721A("YABAI GOBLIN", "YAGO") {
         __baseURI = "";
@@ -21,5 +23,15 @@ contract YabaiGoblin is Ownable, ERC721A {
     function setBaseURI(string calldata uri) external onlyOwner {
         __baseURI = uri;
         emit SetBaseURI(uri);
+    }
+
+    function setMinter(address _minter) external onlyOwner {
+        minter = _minter;
+        emit SetMinter(_minter);
+    }
+
+    function mint(address to, uint256 quantity) external {
+        require(msg.sender == minter);
+        _safeMint(to, quantity);
     }
 }
